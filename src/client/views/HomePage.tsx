fc7bd38c64c12808c20a33b284110adb09bd998d
David Vaughan <dvaug86@gmail.com>
2022-12-12 10:03:33 -0500

    spelling correction

--- src/client/views/HomePage.tsx
+++ src/client/views/HomePage.tsx
@@ -14,8 +14,8 @@ const HomePage: React.FC<HomePageProps> = (props) => { //learn strongtyping prop
                     <div className='info  '>
                         <p className=' indent fs-3 mb-3 '>
                             Hello My name is David Vaughan, and I am a full stack engineer. I am a firm believer that one should always work at improving yourself.
-                            One such way is to keep learning new things. On this site you will find projects of mine that I have created in my pursuit of learning the most cutting-edge techniques
-                            in the programming world. You will find with the portfolio link, both a brief synopsis of what was covered in the project, as well as a link to my creation's GitHub page (unless that information is privileged).  Please feel free to contact me with any questions, comments, or work inquiries.
+                            One such way is to keep learning new things. On this site you will find projects of mine that I have created in my pursuit of learning cutting-edge techniques
+                            in the programming world. You will find with the portfolio link, both a synopsis of what was covered in each project, as well as a link to my creation's GitHub page (unless that information is privileged).  Please feel free to contact me with any questions, or work inquiries.
                         </p>
                         <div className=' d-flex align-items-bottom fs-3 pt-2 '>
                             Thanks and have a great day!
